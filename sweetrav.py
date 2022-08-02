f96f5e9749e1b0c4705964211433d84c1588ca8b
firedragon <firedragon9511@gmail.com>
2022-08-01 23:14:58 -0300

    Commit

--- sweetrav.py
+++ sweetrav.py
@@ -23,9 +23,10 @@ parser = argparse.ArgumentParser(description=banner, formatter_class=RawTextHelp
 parser.add_argument('-p','--path', dest='path', action='store', type=str, help='Specify current folder.', required=False)
 parser.add_argument('-d','--depth', dest='depth', action='store', type=int, help='Generate using depth.', required=False)
 parser.add_argument('-s','--separator', dest='separator', action='store', type=str, help='Custom separator.', required=False, default='/')
-parser.add_argument('-r','--range', dest='range', action='store', type=str, help='Generate a list, ex.: 1-10.', required=False, default='/')
+parser.add_argument('-r','--range', dest='range', action='store', type=str, help='Generate a list. Ex.: 1-10.', required=False, default='/')
 parser.add_argument('-a','--append', dest='append', action='store', type=str, help='Append to final.', required=False, default='')
-parser.add_argument('-f','--fuzz', dest='fuzz', action='store', type=str, help='Fuzz script, ex.: -f "./script.sh FUZZ".', required=False)
+parser.add_argument('-A','--Append', dest='Append', action='store', type=str, help='Append to final using list. Ex.: -r 1-10 -A files.txt', required=False)
+parser.add_argument('-f','--fuzz', dest='fuzz', action='store', type=str, help='Fuzz script. Ex.: -f "./script.sh FUZZ".', required=False)
 
 parser.add_argument('-i', dest='stdin', help='Use stdin pipe.', action='store_true')
 
@@ -66,6 +67,12 @@ def from_list(lst, separator = '/', append = ''):
         fuzz(res)
         print(res)
 
+def read_file(path):
+    f = open(path, 'r')
+    data = f.read().split('\n')
+    f.close()
+    return data
+
 def range_list(rng, separator = '/', append = ''):
     mi = 0
     mx = 0
@@ -77,10 +84,22 @@ def range_list(rng, separator = '/', append = ''):
     else:
         mx = int(rng)
 
+    if args.Append is not None:
+        data = read_file(args.Append)
+        for d in data:
+            for i in range(mi, mx + 1):
+                res = gen_traversal(i, separator) + d
+                fuzz(res)
+                print(res)
+        return
+
+
     for i in range(mi, mx + 1):
         res = gen_traversal(i, separator) + append
         fuzz(res)
         print(res)
+
+    return
 
 
 stdin_input = []
